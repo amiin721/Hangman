61bbe04154cfb386b2a7843d981c7b575d3c6d7e
Amin <mohammedamink4@gmail.com>
2023-05-21 18:01:58 +0530

    commit

--- script.js
+++ script.js
@@ -7,6 +7,30 @@ const finalMessage = document.getElementById('final-message');
 
 const figureParts= document.querySelectorAll(".figure-part");
 
+// const request = new XMLHttpRequest();
+
+// const api = 'http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&minLength=5&maxLength=15&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
+
+// function fetchData() {
+//     const corsProxyUrl = 'https://cors-anywhere.herokuapp.com/';
+   
+//     fetch(corsProxyUrl + api)
+//         .then(response => response.json())
+//         .then(data => {
+//             // Process the JSON response
+//             console.log(data[0].word);
+            
+//         })
+//         .catch(error => {
+//             // Handle any errors
+//             console.log(error);
+//         });
+// }
+
+// function setselectedword() {
+//     console.log(request.response);
+// }
+
 const words = ['apple', 'joker', 'javascript', 'giant', 'baloon' , 'elegant', 'frog', 'whooper' ,'basic'];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
@@ -34,42 +58,11 @@ function displayWord(){
     if(innerWord === selectedWord){
         finalMessage.innerText = 'You Won';
         popup.style.display= 'flex';
+        window.removeEventListener('keydown',disp);
     }
 }
 
-
-function updateWrongLetterE1(){
-    wrongLettersE1.innerHTML = `
-    ${wrongLetters.length > 0 ? '<p>Wrong Letters :</p>' : ''}
-    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
-    `;
-
-    figureParts.forEach((part,index) => {
-        const errors = wrongLetters.length;
-
-        if(index < errors) {
-            part.style.display = 'block'
-        }
-        else{
-            part.style.display = 'none';
-        }
-    });
-
-    if(wrongLetters.length === figureParts.length){
-        finalMessage.innerText = 'You Lost';
-        popup.style.display = 'flex';
-    }
-}
-
-function showNotification(){
-    notification.classList.add('show');
-
-    setTimeout(() => {
-        notification.classList.remove('show');
-    }, 2000);
-}
-
-window.addEventListener('keydown', e =>{
+const disp = (e) =>{
     if(e.keyCode >= 65 && e.keyCode <=90){
         const letter = e.key;
 
@@ -91,11 +84,47 @@ window.addEventListener('keydown', e =>{
             }
         }
     }
-});
+}
+
+window.addEventListener('keydown', disp );
+
+function updateWrongLetterE1(){
+    wrongLettersE1.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Wrong Letters :</p>' : ''}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `;
+
+    figureParts.forEach((part,index) => {
+        const errors = wrongLetters.length;
+
+        if(index < errors) {
+            part.style.display = 'block'
+        }
+        else{
+            part.style.display = 'none';
+        }
+    });
+
+    if(wrongLetters.length === figureParts.length){
+        finalMessage.innerText = `You Lost. The word was "${selectedWord}"\nBetter luck next time!`;
+        popup.style.display = 'flex';
+        window.removeEventListener('keydown',disp);
+    }
+}
+
+function showNotification(){
+    notification.classList.add('show');
+
+    setTimeout(() => {
+        notification.classList.remove('show');
+    }, 2000);
+}
 
 playAgainBtn.addEventListener('click', () => {
     correctLetters.splice(0);
     wrongLetters.splice(0);
+
+    window.addEventListener('keydown', disp );
 
     selectedWord = words[Math.floor(Math.random() * words.length)];
 
@@ -107,4 +136,3 @@ playAgainBtn.addEventListener('click', () => {
 });
 
 displayWord();
-
